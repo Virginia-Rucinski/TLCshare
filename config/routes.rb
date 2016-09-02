e7b9d9c9b932faa6967ce09d67340f9875c6409d
Virginia Rucinski <rucinski9@gmail.com>
2016-09-02 14:50:32 -0400

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   resources :medications
   resources :addresses
   resources :todos
@@ -21,6 +25,7 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
 
