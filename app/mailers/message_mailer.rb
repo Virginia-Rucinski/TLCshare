cd9c78db933346bf332395612453b5e036dee67d
Virginia Rucinski <rucinski9@gmail.com>
2016-09-03 22:36:46 -0400

    Modified Message Contact

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,6 +5,7 @@ class MessageMailer < ApplicationMailer
   def message_me(msg)
     @msg = msg
 
-    mail from: @msg.email, subject: "TLCshare Information Request", body: @msg.content
+    mail from: @msg.email, subject: "TLCshare Information Request: #{@msg.subject}", body: @msg.content
+
   end
 end
